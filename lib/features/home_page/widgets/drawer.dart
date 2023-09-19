6586a16d0e5ebd3643d3a31d9947c4b0265f4efb
bibin k b <bibinkochumalayil007@gmail.com>
2023-09-19 20:11:26 +0530

    drawer updation completed

--- lib/features/home_page/widgets/drawer.dart
+++ lib/features/home_page/widgets/drawer.dart
@@ -1,128 +1,157 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:talkaro/common/widgets/loader.dart';
+import 'package:talkaro/features/authentication/controller/auth_controller.dart';
 import 'package:talkaro/features/login_register/user_information.dart';
 import 'package:talkaro/features/splash_screen/splash_screen.dart';
+import 'package:talkaro/models/user_model.dart';
 import 'package:talkaro/utils/colors.dart';
 import 'package:talkaro/utils/constants.dart';
 
-class CustomDrawer extends StatelessWidget {
-  const CustomDrawer({super.key});
-
+class CustomDrawer extends ConsumerWidget {
   @override
-  Widget build(BuildContext context) {
-    return Drawer(
-      child: ListView(
-        padding: EdgeInsets.zero,
-        children: [
-          DrawerHeader(
-              decoration: BoxDecoration(
-                color: ktheme,
-              ),
-              child: InkWell(
-                onTap: () {
-                  Navigator.of(context).push(MaterialPageRoute(
-                      builder: (context) => UserInformationScreen()));
-                },
+  Widget build(BuildContext context, WidgetRef ref) {
+    return FutureBuilder<UserModel?>(
+      future: ref.read(authControllerProvider).getUserData(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Drawer(
+            child: Loader(),
+          );
+        }
+
+        if (snapshot.hasError) {
+          return const Drawer(
+            child: Text("Error fetching data"),
+          );
+        }
+        final userData = snapshot.data;
+        return Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: [
+              DrawerHeader(
+                decoration: const BoxDecoration(
+                  color: ktheme,
+                ),
                 child: Column(
-                  children: const [
-                    CircleAvatar(
-                      radius: 50,
-                      backgroundImage: AssetImage("images/user.png"),
+                  children: [
+                    InkWell(
+                      onTap: () {
+                        Navigator.of(context).push(MaterialPageRoute(
+                          builder: (context) => const UserInformationScreen(),
+                        ));
+                      },
+                      child: CircleAvatar(
+                        backgroundColor: Colors.grey,
+                        backgroundImage: NetworkImage(userData!.profilePic),
+                        radius: 45,
+                      ),
                     ),
                     kheight10,
                     Text(
-                      'Name',
+                      userData.name,
+                      style: TextStyle(color: kwhite, fontSize: 18),
+                    ),
+                    kheight5,
+                    Text(
+                      userData.phoneNumber,
                       style: TextStyle(
-                        fontSize: 18.5,
+                        color: kwhite,
                       ),
-                    )
+                    ),
                   ],
                 ),
-              )),
-          ListTile(
-            leading: Icon(Icons.group_add),
-            title: Text('New Group'),
-            onTap: () {},
-          ),
-          ListTile(
-            leading: Icon(Icons.person),
-            title: Text('Contacts'),
-            onTap: () {},
-          ),
-          ListTile(
-            leading: Icon(Icons.person_add),
-            title: Text('Invite Friends'),
-            onTap: () {
-              Navigator.pop(context);
-            },
-          ),
-          Divider(
-            thickness: 0.5,
-            indent: 20,
-            endIndent: 20,
-            color: ktheme,
-          ),
-          ListTile(
-            leading: Icon(Icons.privacy_tip),
-            title: Text('privacy policy'),
-            onTap: () {
-              Navigator.pop(context);
-            },
-          ),
-          ListTile(
-            leading: Icon(Icons.info_outline),
-            title: Text('About Us'),
-            onTap: () {
-              Navigator.pop(context);
-            },
-          ),
-          ListTile(
-            leading: Icon(Icons.logout),
-            title: Text('Sign out'),
-            onTap: () {
-              showDialog(
-                context: context,
-                builder: (BuildContext context) {
-                  return AlertDialog(
-                    title: const Text(
-                      'Signout !',
-                      style: TextStyle(color: Colors.red),
-                    ),
-                    content: const Text(
-                        'This action will Signout you from this application.'),
-                    actions: <Widget>[
-                      TextButton(
-                        child: const Text('Cancel'),
-                        onPressed: () {
-                          Navigator.of(context).pop();
-                        },
-                      ),
-                      TextButton(
-                        child: const Text('Yes'),
-                        onPressed: () async {
-                          FirebaseAuth.instance.signOut();
-                          Navigator.pushReplacement(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => const SplashScreen(),
-                            ),
-                          );
-                        },
-                      ),
-                    ],
+              ),
+              ListTile(
+                leading: Icon(Icons.group_add),
+                title: Text('New Group'),
+                onTap: () {},
+              ),
+              ListTile(
+                leading: Icon(Icons.person),
+                title: Text('Contacts'),
+                onTap: () {},
+              ),
+              ListTile(
+                leading: Icon(Icons.person_add),
+                title: Text('Invite Friends'),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+              Divider(
+                thickness: 0.5,
+                indent: 20,
+                endIndent: 20,
+                color: ktheme,
+              ),
+              ListTile(
+                leading: Icon(Icons.privacy_tip),
+                title: Text('privacy policy'),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                leading: Icon(Icons.info_outline),
+                title: Text('About Us'),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                leading: const Icon(
+                  Icons.logout,
+                ),
+                title: const Text(
+                  'Logout',
+                ),
+                onTap: () {
+                  showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return AlertDialog(
+                        title: const Text(
+                          'Signout !',
+                          style: TextStyle(color: Colors.red),
+                        ),
+                        content: const Text(
+                          'This action will Signout you from the application !',
+                          style: TextStyle(
+                              fontSize: 16, fontWeight: FontWeight.w500),
+                        ),
+                        actions: <Widget>[
+                          TextButton(
+                            child: const Text('Cancel'),
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                          ),
+                          TextButton(
+                            child: const Text('Yes'),
+                            onPressed: () async {
+                              FirebaseAuth.instance.signOut();
+
+                              Navigator.pushReplacement(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => const SplashScreen(),
+                                ),
+                              );
+                            },
+                          ),
+                        ],
+                      );
+                    },
                   );
                 },
-              );
-
-              // FirebaseAuth.instance.signOut();
-              // showSnackBar(
-              //     context: context, content: 'You have been signed out !');
-              // Navigator.of(context).pushReplacement(
-              //     MaterialPageRoute(builder: (context) => SplashScreen()));
-            },
+              ),
+            ],
           ),
-        ],
-      ),
+        );
+      },
     );
   }
 }
